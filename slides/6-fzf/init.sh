b65f5db9628b8d1e55c81bf72d16a9af24146d70
Olaf Alders <olaf@wundersolutions.com>
2021-04-25 22:10:31 -0400

    Create symlinks for fd and bat in Ubuntu

--- slides/6-fzf/init.sh
+++ slides/6-fzf/init.sh
@@ -4,6 +4,9 @@ set -eux
 
 touch /root/.bashrc
 
+ln -s /usr/bin/batcat /usr/local/bin/bat
+ln -s /usr/bin/fdfind /usr/local/bin/fd
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
